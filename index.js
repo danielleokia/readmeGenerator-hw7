0c16500d581c4b5f55e1e62576f50c7a9979a7ff
unknown <danielleokia1@gmail.com>
2023-01-03 09:15:42 +0300

    added array of questions

---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer')
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title?'
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project usage?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution info?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github username'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Project Licensing?',
+        choices: ['MIT', 'ISC', 'GNUPLv3'],
+        filter(val){
+            return val.toLowerCase();
+        }
+    },
+];
